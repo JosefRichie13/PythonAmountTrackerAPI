4b9f69ad4197a22bc03397784d04365a21b34cd7
Joseph Richie <joseph.richie@qumu.com>
2024-09-04 12:58:12 +0530

    Added Update Amount Endpoint

--- index.py
+++ index.py
@@ -103,7 +103,7 @@ def addAnExpense(addAnExpenseBody: addAnExpense, response: Response):
         response.status_code = status.HTTP_403_FORBIDDEN
         return {"status": "Expense date of " + addAnExpenseBody.date + " cannot be earlier than amount date of " + convertEpochToDate(amountIDCheck[1])}
     
-    
+
     # Checks the current amount usage
     # We get all the values by the amount ID and add it into summedUpAmount
     # If the supplied expense + summedUpAmount is greater than the amount value, reject with 403
@@ -125,3 +125,61 @@ def addAnExpense(addAnExpenseBody: addAnExpense, response: Response):
     cur.execute(queryToAddAnExpense, valuesToAddAnExpense)
     connection.commit()
     return {"expenseID" : generatedIDForExpense, "status" : "Expense of " + str(addAnExpenseBody.expense) + " added.", "amountID" : addAnExpenseBody.amountID}
+
+
+
+# PUT Body to update an amount
+# The amount is validated to be greater than 0
+class updateAnAmount(BaseModel):
+    amountID : str
+    amountDescription : str
+    amount : float = Field(gt=0, description = "Amount must be greater than 0")
+
+# Update an amount endpoint
+@app.put("/updateAnAmount")
+def updateAnAmount(updateAnAmountBody: updateAnAmount, response: Response):
+
+    # Sanitzes the description 
+    sanitizedDescription = sanitizeString(updateAnAmountBody.amountDescription).strip()
+
+
+    # Checks the amount description, if its empty returns a 400
+    if len(sanitizedDescription) == 0:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {"status" : "Amount description cannot be empty."}
+
+    
+    # Connects to the DB
+    connection = sqlite3.connect("AMOUNTTRACKER.db")
+    cur = connection.cursor()
+
+    # Checks if the supplied amount ID exists in the DB.
+    # If there is no amount ID by that ID, it will return NONE, we return with 404
+    queryToCheckAmountID = "SELECT ID FROM AMOUNTTRACKER WHERE ID = ? AND TYPE = 'AMT'"
+    valuesToCheckAmountID = [updateAnAmountBody.amountID]
+    amountIDCheck = cur.execute(queryToCheckAmountID, valuesToCheckAmountID).fetchone()
+    if amountIDCheck is None:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"status": "No Amount with the ID " + updateAnAmountBody.amountID + " exists. Please recheck"}
+
+
+    # Checks the current amount usage and if its less than the value to be updated
+    # We get all the values by the amount ID and add it into summedUpAmount
+    # If the summedUpAmount is greater than the updated amount value, reject with 403
+    # As we cannot update the amount to less that what is already spent
+    queryToCheckAmountUsage = "SELECT VALUE FROM AMOUNTTRACKER WHERE AMT_ID = ?"
+    valuesToCheckAmountUsage = [updateAnAmountBody.amountID]
+    currentAmountCheck = cur.execute(queryToCheckAmountUsage, valuesToCheckAmountUsage).fetchall()
+    extractedNumbers = [number[0] for number in currentAmountCheck]
+    summedUpAmount = sum(extractedNumbers)
+    if summedUpAmount > updateAnAmountBody.amount:
+        response.status_code = status.HTTP_403_FORBIDDEN
+        return {"status": "Total expense for this amount is " + str(summedUpAmount) + ". Cannot update the amount to anything below."}
+
+
+    # Updates the amount description and value into the DB for the supplied amount ID
+    queryToUpdateAnAmount = "UPDATE AMOUNTTRACKER SET AMT_EXP_DESC = ?, VALUE = ? WHERE ID = ?"
+    valuesToUpdateAnAmount = (sanitizedDescription, updateAnAmountBody.amount, updateAnAmountBody.amountID)
+    cur.execute(queryToUpdateAnAmount, valuesToUpdateAnAmount)
+    connection.commit()
+    return {"amountID" : updateAnAmountBody.amountID, "status" : "Amount updated."}
